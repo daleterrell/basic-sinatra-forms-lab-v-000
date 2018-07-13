80b088f5c86ea327364d56f28f4a963b0852a9c4
Dale Terrell <tragicid@gmail.com>
2018-07-13 10:26:14 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,6 +2,11 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
   GET Team_name('name')
-  GET 
+  GET Coach ('coach')
+  GET Point Guard ('pg')
+  GET Shooting Guard ('sg') 
+  GET Power Forward ('pf')
+  GET Small Forward ('sf')
+  GET Center ('c') 
 
 end
